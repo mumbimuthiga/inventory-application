117dbe0d14bf9304de97745ffa049d2cda1702c7
Mumbi <veronicmuthiga@gmail.com>
2020-12-07 11:47:26 +0300

    Add the following

--- express-inventoryapplication/controllers/genreController.js
+++ express-inventoryapplication/controllers/genreController.js
@@ -9,7 +9,7 @@ exports.genre_list=function(req,res){
    .exec(function(err ,list_genre){
        if(err) {return next (err);}
        //Successful so render
-       res.render( genre_list ,{title :'Genre List' ,genre_list:list_genre})
+       res.render( 'genre_list' ,{title :'Genre List' ,genre_list:list_genre})
    })
 };
 //Display detail page of every genre
